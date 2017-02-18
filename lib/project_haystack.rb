763a7942eeb22aca65044c0c89451ad72c0d0295
Petersen, Anya <Anya.Petersen@nrel.gov>
2017-02-17 18:26:02 -0700

    More cleanup.

--- lib/project_haystack.rb
+++ lib/project_haystack.rb
@@ -1,24 +1,3 @@
-# module ProjectHaystack
-#   require 'faraday'
-#   # took this from mongoid
-#   # def configure
-#   #   block_given? ? yield(Config) : Config
-#   # end
-#   # # For now, just make it work
-#   # BASE_URL = 'skyspark-ops.nrel.gov'
-
-#   # headers = ['Accept: application/json','Authorization: Basic YmVuOg==\n']
-#   # def ops
-
-#   #   # ops_url = "https://#{CREDENTIALS}@#{BASE_URL}/api/#{PROJECT}/ops"
-#   #   # puts "checking ops path at #{ops_url}"
-#   #   c = Config.connection('demo')
-#   #   response = c.get("/ops")
-#   # end
-
-  
-
-# end
 require 'faraday'
 require 'project_haystack/config'
 require 'project_haystack/point'
@@ -26,7 +5,6 @@ require 'project_haystack/project'
 require 'project_haystack/range'
 require 'project_haystack/timestamp'
 require 'project_haystack/auth/conversation'
-# require 'project_haystack/auth/scram'
 require 'active_support/core_ext/hash'
 if defined?(Rails)
   require "project_haystack/railtie"
